8534446cb34db8f80eaada3c2d721646f5439973
cloudhunternet <cloudhunternet@4ebda398-8e3a-11de-a708-79fcdc47a425>
2009-08-28 03:37:45 +0000

    change seek() to expect performance improvement.

--- src/net/cloudhunter/compat/java/io/RandomAccessFile.java
+++ src/net/cloudhunter/compat/java/io/RandomAccessFile.java
@@ -10,6 +10,9 @@ public class RandomAccessFile {
 	private FileConnection _conn = null;
 	private String _path = null;
 	private DataInputStream _stream = null;
+
+	/** 開いている_streamの現在位置 */
+	private long currentPos;
 	
 	public RandomAccessFile(File file, String mode) throws FileNotFoundException {
 		try {
@@ -19,27 +22,40 @@ public class RandomAccessFile {
 			if(!_conn.exists()) {
 				throw new FileNotFoundException(_path);
 			}
-			_stream = _conn.openDataInputStream(); 
+			_stream = _conn.openDataInputStream();
+			currentPos = 0;
+			if(_stream.markSupported()) {
+				_stream.mark(Integer.MAX_VALUE);	// 2,147,483,647 (BlackBerry Bold)  
+			}
 		} catch(IOException e) {
 			//TODO this works?
 			throw new RuntimeException(e.getMessage());
 		}
 	}
      
-     
      public long length() throws IOException {
     	 return _conn.fileSize();
      }
      
      public void seek(long pos) throws IOException {
-    	 _stream.close();
-    	 _stream = _conn.openDataInputStream();
-    	 _stream.skip(pos);
+    	 if(currentPos < pos) {
+    		 _stream.skip(pos - currentPos);
+    	 } else if (currentPos > pos) {
+    		 if(_stream.markSupported() && pos < Integer.MAX_VALUE) {
+    			 _stream.reset();
+    		 } else {
+    			 _stream.close();
+    			 _stream = _conn.openDataInputStream();
+    		 }
+			 _stream.skip(pos);
+    	 }
+		 currentPos = pos;
      }
      
      
 	public void close() throws IOException {
 		System.out.println("Closing RandomAccessFile:" + _path);
+		currentPos = 0;
 		if(_stream != null) {
 			_stream.close();
 			_stream = null;
@@ -51,10 +67,13 @@ public class RandomAccessFile {
 	}
 	
 	public int read(byte[] b, int off, int len) throws IOException {
-		return _stream.read(b, off, len);
+		int size = _stream.read(b, off, len);
+		currentPos += size;
+		return size;
 	}
      
      public final void readFully(byte[] b, int off, int len) throws IOException {
     	 _stream.readFully(b, off, len);
+    	 currentPos += len;
      }
 }
