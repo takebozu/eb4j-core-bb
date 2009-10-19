b58dbdcd6c8ba1c33b3932eca81d26872a106ad5
cloudhunternet <cloudhunternet@4ebda398-8e3a-11de-a708-79fcdc47a425>
2009-10-19 03:08:11 +0000

    Make it compatible with EBZip level 4-5. Previously is was compatible level 0-3.

--- src/fuku/eb4j/io/EBZipConstants.java
+++ src/fuku/eb4j/io/EBZipConstants.java
@@ -10,7 +10,7 @@ public interface EBZipConstants {
     /** EBZIP形式のヘッダサイズ */
     int EBZIP_HEADER_SIZE = 22;
     /** EBZIP形式の最大圧縮レベル */
-    int EBZIP_MAX_LEVEL = 3;
+    int EBZIP_MAX_LEVEL = 5;
     /** EBZIP形式のデフォルト圧縮レベル */
     int EBZIP_DEFAULT_LEVEL = 0;
 }
