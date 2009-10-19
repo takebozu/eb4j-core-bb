ac76d8722a71e84be0946d30aa9a1c1245e3d160
cloudhunternet <cloudhunternet@4ebda398-8e3a-11de-a708-79fcdc47a425>
2009-10-19 18:01:05 +0000

    V0.2.0 Released

---
+++ tags/V0.2.0/src/fuku/eb4j/io/HuffmanNode.java
@@ -0,0 +1,192 @@
+package fuku.eb4j.io;
+
+import net.cloudhunter.compat.java.lang.Comparable;
+
+/**
+ * ハフマンノードクラス。
+ *
+ * @author Hisaya FUKUMOTO
+ */
+public class HuffmanNode implements Comparable {
+
+    /** EOF葉ノード */
+    protected static final int LEAF_EOF = 0;
+    /** 8bit葉ノード */
+    protected static final int LEAF_8 = 1;
+    /** 16bit葉ノード */
+    protected static final int LEAF_16 = 2;
+    /** 32bit葉ノード */
+    protected static final int LEAF_32 = 3;
+
+    /** 葉ノードの種類 */
+    private int _leafType = -1;
+    /** 値 */
+    private long _value = -1L;
+    /** 出現頻度値 */
+    private int _frequency = 0;
+    /** 左子ノード */
+    private HuffmanNode _left = null;
+    /** 右子ノード */
+    private HuffmanNode _right = null;
+
+    /** Vector中のインデックス（HuffmanTree作成用） */
+    private int index = -1;
+    
+    /** Vectorに格納したときのインデックス（HuffmanTree作成用） */
+    private int orgIndex = -1;
+
+    /**
+     * コンストラクタ。 (葉ノード用)
+     *
+     * @param value 値
+     * @param frequency 出現頻度値
+     * @param leafType 葉ノードの種類
+     */
+    protected HuffmanNode(long value, int frequency, int leafType) {
+        _value = value;
+        _frequency = frequency;
+        _leafType = leafType;
+    }
+
+    /**
+     * コンストラクタ。 (枝ノード用)
+     *
+     * @param left 左子ノード
+     * @param right 右子ノード
+     */
+    protected HuffmanNode(HuffmanNode left, HuffmanNode right) {
+        _left = left;
+        _right = right;
+        _frequency = _left.getFrequency() + _right.getFrequency();
+    }
+
+    //--------------------------------------------------------------------------------
+    // setters/getters
+    //--------------------------------------------------------------------------------
+    /**
+     * 葉ノードの種類を返します。
+     *
+     * @return 葉ノードの種類
+     */
+    protected int getLeafType() {
+        return _leafType;
+    }
+
+    /**
+     * 値を返します。
+     *
+     * @return 値
+     */
+    protected long getValue() {
+        return _value;
+    }
+
+    /**
+     * 出現頻度値を返します。
+     *
+     * @return 出現頻度値
+     */
+    protected int getFrequency() {
+        return _frequency;
+    }
+
+    /**
+     * 左子ノードを返します。
+     *
+     * @return 左子ノード
+     */
+    protected HuffmanNode getLeft() {
+        return _left;
+    }
+
+    /**
+     * 右子ノードを返します。
+     *
+     * @return 右子ノード
+     */
+    protected HuffmanNode getRight() {
+        return _right;
+    }
+
+    /**
+     * 葉ノードか枝ノードかを判別します。
+     *
+     * @return 葉ノードであればtrue、枝ノードであればfalse
+     */
+    protected boolean isLeaf() {
+        if (_right == null && _left == null) {
+            return true;
+        }
+        return false;
+    }
+
+    public void setIndex(int index) {
+    	this.index = index;
+    }
+
+    public int getIndex() {
+    	return index;
+    }
+    
+    public void setOrgIndex(int index) {
+    	this.orgIndex = index;
+    }
+    
+    public int getOrgIndex() {
+    	return orgIndex;
+    }
+
+    //--------------------------------------------------------------------------------
+    
+    /**
+     * オブジェクトのハッシュコード値を返します。
+     *
+     * @return ハッシュコード値
+     */
+    public int hashCode() {
+        return (int)(_value + _frequency);
+    }
+
+    /**
+     * このオブジェクトとほかのオブジェクトが等しいかどうかを返します。
+     *
+     * @param obj 比較対象オブジェクト
+     * @return 等しい場合はtrue、そうでない場合はfalse
+     */
+    public boolean equals(Object obj) {
+        if (obj instanceof HuffmanNode) {
+            HuffmanNode node = (HuffmanNode)obj;
+            if (node.getLeafType() == getLeafType()
+                && node.getValue() == getValue()
+                && node.getFrequency() == getFrequency()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 2つのノードの出現頻度値を比較します。
+     *
+     * @param node 比較対象のノード
+     * @return このノードの頻度値が引数ノードの頻度値と等しい場合0、
+     *         このノードの頻度値が引数ノードの頻度値より小さい場合は0より小さい値、
+     *         このノードの頻度値が引数ノードの頻度値より大きい場合は0より大きい値
+     */
+//    @Override
+    public int compareTo(Object node) {
+        int ret = getFrequency() - ((HuffmanNode)node).getFrequency();
+        return ret;
+    }
+    
+    /**
+     * 文字列表現を返します。
+     */
+    public String toString() {
+    	return "freqency=" + _frequency
+    		+ "\tvalue=" + _value
+    		+ "\tleafType=" + _leafType;
+    }
+}
+
+// end of HuffmanNode.java
---
+++ tags/V0.2.0/src/net/cloudhunter/compat/java/io/File.java
@@ -0,0 +1,163 @@
+package net.cloudhunter.compat.java.io;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.Enumeration;
+
+import javax.microedition.io.Connector;
+import javax.microedition.io.file.FileConnection;
+
+import net.cloudhunter.compat.java.util.ArrayList;
+import net.cloudhunter.compat.java.util.List;
+
+
+public class File {
+	private String _pathname = null;
+//	private File _parent = null;
+//	private String _child = null;
+	
+	public static final char PATH_SEPARATOR = '/';
+	
+	public File(String pathname) {
+		_pathname = urlEncode(pathname);
+	}
+	
+	public File(File parent, String child) {
+		_pathname = parent.getPath() + PATH_SEPARATOR + urlEncode(child);
+	}
+	
+	/**
+	 * FileConnectionを取得する。
+	 * @return
+	 * @throws IOException
+	 */
+	private FileConnection getFileConnection() throws IOException {
+		return (FileConnection)Connector.open(_pathname);
+	}
+	
+	public boolean canRead() {
+		boolean result = false;
+		FileConnection conn = null;
+		try {
+			conn = getFileConnection();
+			result = conn.canRead();
+		} catch(IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(conn != null) {
+					conn.close();
+				}
+			} catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return result;
+	}
+	
+	public String[] list() {
+		List results = new ArrayList();
+		FileConnection conn = null;
+		try {
+			conn = getFileConnection();
+			Enumeration list = conn.list();
+			while(list.hasMoreElements()) {
+				String file = (String)list.nextElement();
+				if (file.endsWith("/")) {
+					//ディレクトリの場合は互換性のため最後の/を取る
+					results.add(file.substring(0, file.length() - 1));
+				} else {
+					results.add(file);
+				}
+			}
+		} catch(IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(conn != null) {
+					conn.close();
+				}
+			} catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return (String[])results.toArray(new String[results.size()]);
+	}
+	
+	public File[] listFiles() {
+		String[] list = list();
+		File[] files = new File[list.length];
+		for(int i=0; i<list.length; i++) {
+			files[i] = new File(this, list[i]);
+		}
+		return files;
+	}
+	
+	public boolean isDirectory() {
+		boolean result = false;
+		FileConnection conn = null;
+		try {
+			conn = getFileConnection();
+			result = conn.isDirectory();
+		} catch(IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(conn != null) {
+					conn.close();
+				}
+			} catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return result;
+	}
+	
+	public String getName() {
+		return _pathname.substring(_pathname.lastIndexOf(PATH_SEPARATOR));
+	}
+	
+	public String getPath() {
+		return _pathname;
+	}
+	
+	/**
+	 * encode URL
+	 * @param value url
+	 * @return encoded url
+	 */
+	private static String urlEncode(String value) {
+		StringBuffer buf = new StringBuffer();
+		byte[] utf;
+		try {
+			utf = value.getBytes("UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			utf = value.getBytes(); //基本的にはここには来ない
+		}
+		for (int i = 0; i < utf.length; ++i) {
+			char b = (char) utf[i];
+			if (b == ' ') {
+				buf.append('+');
+			} else if (isRFC3986Unreserved(b)) {
+				buf.append(b);
+			} else {
+				buf.append('%');
+				buf.append(Integer.toHexString(b & 0xff).toUpperCase()); // u.c. per RFC 3986
+			}
+		}
+		return buf.toString();
+	}
+
+	/**
+	 * エンコードすべき文字かどうかを判定
+	 * @param b
+	 * @return エンコードすべき文字の場合true
+	 */
+	private static boolean isRFC3986Unreserved(char b) {
+		return (b >= 'A' && b <= 'Z')
+			|| (b >= 'a' && b <= 'z')
+			|| Character.isDigit(b)
+			|| ".-~_/:%".indexOf(b) >= 0;
+	}
+}
