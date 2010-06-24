187b6712ebdade040cfc61c78ea9ee775fe954a6
cloudhunternet <cloudhunternet@4ebda398-8e3a-11de-a708-79fcdc47a425>
2010-06-24 16:16:01 +0000

    implements 4bitRLE/8bitRLE uncomporess methods.

--- src/fuku/eb4j/util/ImageUtil.java
+++ src/fuku/eb4j/util/ImageUtil.java
@@ -1,5 +1,7 @@
 package fuku.eb4j.util;
 
+import net.rim.device.api.util.Arrays;
+
 //import java.awt.Color;
 //import java.util.Arrays;
 //import java.util.zip.CRC32;
@@ -455,177 +457,177 @@ public class ImageUtil {
 //        return _encodePNG(width, height, image, level);
 //    }
 //
-//    /**
-//     * 8bitランレングスエンコードされたDIBデータを伸張します。
-//     *
-//     * @param rle RLE-DIBデータ
-//     * @param off イメージデータの位置
-//     * @param width 幅
-//     * @param height 高さ
-//     * @return 伸張したDIBデータ (ヘッダはそのまま)
-//     */
-//    private static byte[] _expandRLE8(byte[] rle, int off, int width, int height) {
-//        // 1行のバイト数の算出
-//        int lineBytes = (width * 8 + 31) / 32 * 4;
-//        // 展開後のサイズ
-//        int size = off + lineBytes * height;
-//        byte[] dib = new byte[size];
-//        Arrays.fill(dib, (byte)0x00);
-//        System.arraycopy(rle, 0, dib, 0, off); // ヘッダはそのままコピー
-//
-//        int sidx = off;
-//        for (int y=0; y<height; y++) {
-//            int didx = off + lineBytes * y;
-//            int x = 0;
-//            while (x < width) {
-//                int code1 = rle[sidx++] & 0xff; // インデックス数
-//                int code2 = rle[sidx++] & 0xff; // インデックス
-//                // エスケープコード
-//                if (code1 == 0x00) {
-//                    boolean eol = false;
-//                    switch (code2) {
-//                        case 0x00: // end of line
-//                            if (x > 0) {
-//                                eol = true;
-//                            }
-//                            break;
-//                        case 0x01: // end of block
-//                            return dib;
-//                        case 0x02: // skip
-//                            code1 = rle[sidx++] & 0xff;
-//                            code2 = rle[sidx++] & 0xff;
-//                            x += code1;
-//                            y += code2;
-//                            didx += code1 + lineBytes * code2;
-//                            break;
-//                        default: // absolute mode
-//                            x += code2;
-//                            for (int i=0; i<code2; i++) {
-//                                dib[didx++] = rle[sidx++];
-//                            }
-//                            if ((code2%2) != 0) {
-//                                sidx++;
-//                            }
-//                            break;
-//                    }
-//                    if (eol) {
-//                        break;
-//                    }
-//                } else {
-//                    x += code1;
-//                    for (int i=0; i<code1; i++) {
-//                        dib[didx++] = (byte)code2;
-//                    }
-//                }
-//            }
-//        }
-//        return dib;
-//    }
-//
-//    /**
-//     * 4bitランレングスエンコードされたDIBデータを伸張します。
-//     *
-//     * @param rle RLE-DIBデータ
-//     * @param off イメージデータの位置
-//     * @param width 幅
-//     * @param height 高さ
-//     * @return 伸張したDIBデータ (ヘッダはそのまま)
-//     */
-//    private static byte[] _expandRLE4(byte[] rle, int off, int width, int height) {
-//        // 1行のバイト数の算出
-//        int lineBytes = (width * 4 + 31) / 32 * 4;
-//        // 展開後のサイズ
-//        int size = off + lineBytes * height;
-//        byte[] dib = new byte[size];
-//        Arrays.fill(dib, (byte)0x00);
-//        System.arraycopy(rle, 0, dib, 0, off); // ヘッダはそのままコピー
-//
-//        int sidx = off;
-//        for (int y=0; y<height; y++) {
-//            int didx = off + lineBytes * y;
-//            boolean high = true;
-//            int x = 0;
-//            while (x < width) {
-//                int code1 = rle[sidx++] & 0xff; // インデックス数
-//                int code2 = rle[sidx++] & 0xff; // インデックス
-//                // エスケープコード
-//                if (code1 == 0x00) {
-//                    boolean eol = false;
-//                    switch (code2) {
-//                        case 0x00: // end of line
-//                            if (x > 0) {
-//                                eol = true;
-//                            }
-//                            break;
-//                        case 0x01: // end of block
-//                            return dib;
-//                        case 0x02: { // skip
-//                            code1 = rle[sidx++] & 0xff;
-//                            code2 = rle[sidx++] & 0xff;
-//                            x += code1;
-//                            y += code2;
-//                            didx += code1 / 2 + lineBytes * code2;
-//                            if ((code1%2) != 0) {
-//                                high = !high;
-//                                if (high) {
-//                                    didx++;
-//                                }
-//                            }
-//                            break;
-//                        }
-//                        default: // absolute mode
-//                            x += code2;
-//                            int cnt = (code2 + 1) / 2;
-//                            if (high) {
-//                                for (int i=0; i<cnt; i++) {
-//                                    dib[didx++] = rle[sidx++];
-//                                }
-//                                if ((code2%2) != 0) {
-//                                    didx--;
-//                                    dib[didx] &= 0xf0;
-//                                    high = false;
-//                                }
-//                            } else {
-//                                for (int i=0; i<cnt; i++) {
-//                                    dib[didx++] |= (rle[sidx] >>> 4) & 0x0f;
-//                                    dib[didx] |= (rle[sidx++] << 4) & 0xf0;
-//                                }
-//                                if ((code2%2) != 0) {
-//                                    dib[didx] = (byte)0x00;
-//                                    high = true;
-//                                }
-//                            }
-//                            if ((cnt%2) != 0) {
-//                                sidx++;
-//                            }
-//                            break;
-//                    }
-//                    if (eol) {
-//                        break;
-//                    }
-//                } else {
-//                    x += code1;
-//                    if (!high) {
-//                        dib[didx++] = (byte)((code2 >>> 4) & 0x0f);
-//                        code2 = ((code2 >>> 4) & 0x0f) | ((code2 << 4) & 0xf0);
-//                        code1--;
-//                        high = true;
-//                    }
-//                    int cnt = (code1 + 1) / 2;
-//                    for (int i=0; i<cnt; i++) {
-//                        dib[didx++] = (byte)code2;
-//                    }
-//                    if ((code1%2) != 0) {
-//                        didx--;
-//                        dib[didx] &= 0xf0;
-//                        high = false;
-//                    }
-//                }
-//            }
-//        }
-//        return dib;
-//    }
-//
+    /**
+     * 8bitランレングスエンコードされたDIBデータを伸張します。
+     *
+     * @param rle RLE-DIBデータ
+     * @param off イメージデータの位置
+     * @param width 幅
+     * @param height 高さ
+     * @return 伸張したDIBデータ (ヘッダはそのまま)
+     */
+    public static byte[] expandRLE8(byte[] rle, int off, int width, int height) {
+        // 1行のバイト数の算出
+        int lineBytes = (width * 8 + 31) / 32 * 4;
+        // 展開後のサイズ
+        int size = off + lineBytes * height;
+        byte[] dib = new byte[size];
+        Arrays.fill(dib, (byte)0x00);
+        System.arraycopy(rle, 0, dib, 0, off); // ヘッダはそのままコピー
+
+        int sidx = off;
+        for (int y=0; y<height; y++) {
+            int didx = off + lineBytes * y;
+            int x = 0;
+            while (x < width) {
+                int code1 = rle[sidx++] & 0xff; // インデックス数
+                int code2 = rle[sidx++] & 0xff; // インデックス
+                // エスケープコード
+                if (code1 == 0x00) {
+                    boolean eol = false;
+                    switch (code2) {
+                        case 0x00: // end of line
+                            if (x > 0) {
+                                eol = true;
+                            }
+                            break;
+                        case 0x01: // end of block
+                            return dib;
+                        case 0x02: // skip
+                            code1 = rle[sidx++] & 0xff;
+                            code2 = rle[sidx++] & 0xff;
+                            x += code1;
+                            y += code2;
+                            didx += code1 + lineBytes * code2;
+                            break;
+                        default: // absolute mode
+                            x += code2;
+                            for (int i=0; i<code2; i++) {
+                                dib[didx++] = rle[sidx++];
+                            }
+                            if ((code2%2) != 0) {
+                                sidx++;
+                            }
+                            break;
+                    }
+                    if (eol) {
+                        break;
+                    }
+                } else {
+                    x += code1;
+                    for (int i=0; i<code1; i++) {
+                        dib[didx++] = (byte)code2;
+                    }
+                }
+            }
+        }
+        return dib;
+    }
+
+    /**
+     * 4bitランレングスエンコードされたDIBデータを伸張します。
+     *
+     * @param rle RLE-DIBデータ
+     * @param off イメージデータの位置
+     * @param width 幅
+     * @param height 高さ
+     * @return 伸張したDIBデータ (ヘッダはそのまま)
+     */
+    public static byte[] expandRLE4(byte[] rle, int off, int width, int height) {
+        // 1行のバイト数の算出
+        int lineBytes = (width * 4 + 31) / 32 * 4;
+        // 展開後のサイズ
+        int size = off + lineBytes * height;
+        byte[] dib = new byte[size];
+        Arrays.fill(dib, (byte)0x00);
+        System.arraycopy(rle, 0, dib, 0, off); // ヘッダはそのままコピー
+
+        int sidx = off;
+        for (int y=0; y<height; y++) {
+            int didx = off + lineBytes * y;
+            boolean high = true;
+            int x = 0;
+            while (x < width) {
+                int code1 = rle[sidx++] & 0xff; // インデックス数
+                int code2 = rle[sidx++] & 0xff; // インデックス
+                // エスケープコード
+                if (code1 == 0x00) {
+                    boolean eol = false;
+                    switch (code2) {
+                        case 0x00: // end of line
+                            if (x > 0) {
+                                eol = true;
+                            }
+                            break;
+                        case 0x01: // end of block
+                            return dib;
+                        case 0x02: { // skip
+                            code1 = rle[sidx++] & 0xff;
+                            code2 = rle[sidx++] & 0xff;
+                            x += code1;
+                            y += code2;
+                            didx += code1 / 2 + lineBytes * code2;
+                            if ((code1%2) != 0) {
+                                high = !high;
+                                if (high) {
+                                    didx++;
+                                }
+                            }
+                            break;
+                        }
+                        default: // absolute mode
+                            x += code2;
+                            int cnt = (code2 + 1) / 2;
+                            if (high) {
+                                for (int i=0; i<cnt; i++) {
+                                    dib[didx++] = rle[sidx++];
+                                }
+                                if ((code2%2) != 0) {
+                                    didx--;
+                                    dib[didx] &= 0xf0;
+                                    high = false;
+                                }
+                            } else {
+                                for (int i=0; i<cnt; i++) {
+                                    dib[didx++] |= (rle[sidx] >>> 4) & 0x0f;
+                                    dib[didx] |= (rle[sidx++] << 4) & 0xf0;
+                                }
+                                if ((code2%2) != 0) {
+                                    dib[didx] = (byte)0x00;
+                                    high = true;
+                                }
+                            }
+                            if ((cnt%2) != 0) {
+                                sidx++;
+                            }
+                            break;
+                    }
+                    if (eol) {
+                        break;
+                    }
+                } else {
+                    x += code1;
+                    if (!high) {
+                        dib[didx++] = (byte)((code2 >>> 4) & 0x0f);
+                        code2 = ((code2 >>> 4) & 0x0f) | ((code2 << 4) & 0xf0);
+                        code1--;
+                        high = true;
+                    }
+                    int cnt = (code1 + 1) / 2;
+                    for (int i=0; i<cnt; i++) {
+                        dib[didx++] = (byte)code2;
+                    }
+                    if ((code1%2) != 0) {
+                        didx--;
+                        dib[didx] &= 0xf0;
+                        high = false;
+                    }
+                }
+            }
+        }
+        return dib;
+    }
+
 //    /**
 //     * イメージデータをPNGにエンコードします。
 //     *
